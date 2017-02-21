23cd0617d0f4f0871c0f69c8d00568b258273363
ChiHoc <genioschen@gmail.com>
2017-02-21 18:29:01 +0800

    整理代码
    整理代码

--- wxanalysis_py3.py
+++ wxanalysis_py3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-  
+#-*-coding:utf-8-*-
 
 import urllib
 import os
@@ -13,22 +13,25 @@ from tkinter import messagebox
 from datetime import *
 from pycookiecheat import chrome_cookies
 
-regularStyle = xlwt.easyxf('alignment: horiz centre; font: name Microsoft YaHei')
+regularStyle = xlwt.easyxf(
+    'alignment: horiz centre; font: name Microsoft YaHei')
 
 window = Tk()
 
 # 检查请求次数，返回响应日期
+
+
 def getRequestCount(begin_date, end_date):
 
     beginDate = datetime.strptime(begin_date, '%Y-%m-%d')
 
     endDate = datetime.strptime(end_date, '%Y-%m-%d')
 
-    count = math.ceil((endDate - beginDate).days / 100.0);
+    count = math.ceil((endDate - beginDate).days / 100.0)
 
     dates = []
 
-    for index in range(0, int(count) - 1) :
+    for index in range(0, int(count) - 1):
 
         beginDate = (beginDate + timedelta(days=100))
 
@@ -41,6 +44,8 @@ def getRequestCount(begin_date, end_date):
     return dates
 
 # 获取含头部的sheet
+
+
 def getSheet(begin_date, end_date):
 
     wb = xlwt.Workbook()
@@ -67,9 +72,11 @@ def getSheet(begin_date, end_date):
 
     return wb, sheet
 
+
 def saveSheet(wb, name):
 
     wb.save(name + '.xls')
+
 
 def writeSheet(sheet, start, items):
 
@@ -77,7 +84,7 @@ def writeSheet(sheet, start, items):
 
     index = start
 
-    for item in items :
+    for item in items:
 
         sheet.write(index, 0, item['publish_date'], regularStyle)
 
@@ -104,25 +111,36 @@ def writeSheet(sheet, start, items):
     return index
 
 # 获取微信数据
+
+
 def getWechatData(begin_date, end_date, token):
 
     print(u'== 开始请求 ==')
 
     url = 'http://mp.weixin.qq.com/misc/appmsganalysis'
 
-    params = {'action':'all','begin_date':begin_date,'end_date':end_date,'order_by':'1','order_direction':'1','token':token,'lang':'zh_CN','f':'json','ajax':'1'}
+    params = {'action': 'all',
+              'begin_date': begin_date,
+              'end_date': end_date,
+              'order_by': '1',
+              'order_direction': '1',
+              'token': token,
+              'lang': 'zh_CN',
+              'f': 'json',
+              'ajax': '1'}
 
     headers = {
         'cache-control': 'no-cache',
-        }
+    }
 
     cookies = chrome_cookies('http://mp.weixin.qq.com')
 
-    response = requests.request('GET', url, headers=headers, params=params, cookies=cookies)
+    response = requests.request(
+        'GET', url, headers=headers, params=params, cookies=cookies)
 
     respJson = json.loads(response.text)
 
-    if respJson['base_resp']['ret'] == 0 :
+    if respJson['base_resp']['ret'] == 0:
 
         items = json.loads(respJson['total_article_data'])['list']
 
@@ -130,46 +148,49 @@ def getWechatData(begin_date, end_date, token):
 
         return items
 
-    elif respJson['base_resp']['ret'] == -1 :
-     
+    elif respJson['base_resp']['ret'] == -1:
+
         print(u'== 请使用Chrome到mp.weixin.qq.com进行登录 ==')
 
-        messagebox.showinfo(title = u'温馨提示', message = u'请使用Chrome到mp.weixin.qq.com进行登录')
+        messagebox.showinfo(
+            title=u'温馨提示', message=u'请使用Chrome到mp.weixin.qq.com进行登录')
 
         return -1
 
-    elif respJson['base_resp']['ret'] == 200040 :
+    elif respJson['base_resp']['ret'] == 200040:
 
         print(u'== Token错误 ==')
 
         print(respJson)
 
-        messagebox.showinfo(title = u'Token错误', message = u'请重新填写Token')
+        messagebox.showinfo(title=u'Token错误', message=u'请重新填写Token')
 
         return -1
-        
-    elif respJson['base_resp']['ret'] == 200003 :
+
+    elif respJson['base_resp']['ret'] == 200003:
 
         print(u'== Session过期，请重新登录 ==')
 
         print(respJson)
 
-        messagebox.showinfo(title = u'Session过期', message = u'请重新登录')
+        messagebox.showinfo(title=u'Session过期', message=u'请重新登录')
 
         return -1
 
-    else :
+    else:
 
         print(u'== 未知错误，请联系陈艾森 ==')
 
-        messagebox.showinfo(title = u'未知错误', message = u'请联系陈艾森')
+        messagebox.showinfo(title=u'未知错误', message=u'请联系陈艾森')
 
         return -1
 
 # 开始请求数据
-def runRequestData(begin_date, end_date, token) :
 
-    wb, sheet = getSheet(begin_date, end_date);
+
+def runRequestData(begin_date, end_date, token):
+
+    wb, sheet = getSheet(begin_date, end_date)
 
     index = 1
 
@@ -177,15 +198,16 @@ def runRequestData(begin_date, end_date, token) :
 
     startTime = begin_date
 
-    for pos in range(0, len(requestDates)): 
+    for pos in range(0, len(requestDates)):
 
         date = requestDates[pos]
 
-        print(u'== 第 ' + str(pos + 1) + u' 次 : ' + startTime + u' - ' + date + u' ==')
+        print(u'== 第 ' + str(pos + 1) + u' 次 : ' +
+              startTime + u' - ' + date + u' ==')
 
         items = getWechatData(startTime, date, token)
 
-        if items == -1 :
+        if items == -1:
 
             return
 
@@ -197,10 +219,13 @@ def runRequestData(begin_date, end_date, token) :
 
     print(u'== 执行成功 ==')
 
-    messagebox.showinfo(title = u'执行成功', message = u'文件保存为 ' + begin_date + '-' + end_date + u'.xls')
+    messagebox.showinfo(title=u'执行成功', message=u'文件保存为 ' +
+                        begin_date + '-' + end_date + u'.xls')
 
-#初始化输入框
-def initInputWindow() :
+# 初始化输入框
+
+
+def initInputWindow():
 
     window.title(u'微信统计')
 
@@ -211,18 +236,18 @@ def initInputWindow() :
     beginInput.set('2017-01-01')
     endInput.set('2017-01-15')
 
-    Label(window, text = u'开始时间：').grid(row = 0)
-    Label(window, text = u'结束时间：').grid(row = 1)
-    Label(window, text = u'Token：').grid(row = 2)
+    Label(window, text=u'开始时间：').grid(row=0)
+    Label(window, text=u'结束时间：').grid(row=1)
+    Label(window, text=u'Token：').grid(row=2)
 
-    Entry(window, width=20, textvariable=beginInput).grid(row = 0, column = 1)
-    Entry(window, width=20, textvariable=endInput).grid(row = 1, column = 1)
-    Entry(window, width=20, textvariable=tokenInput).grid(row = 2, column = 1)
+    Entry(window, width=20, textvariable=beginInput).grid(row=0, column=1)
+    Entry(window, width=20, textvariable=endInput).grid(row=1, column=1)
+    Entry(window, width=20, textvariable=tokenInput).grid(row=2, column=1)
 
-    run = Button(window, text=u'执行', command=lambda : runRequestData(beginInput.get(), endInput.get(), tokenInput.get())).grid(row = 3, columnspan=2)
+    Button(window, text=u'执行', command=lambda: runRequestData(
+        beginInput.get(), endInput.get(), tokenInput.get())).grid(row=3, columnspan=2)
 
     window.mainloop()
 
 
 initInputWindow()
-
