50d1676c500389dc81301be8b3585e8245d5d7e5
ChiHoc <genioschen@gmail.com>
2017-04-14 11:14:39 +0800

    修改测试
    修改测试

--- test_py3.py
+++ test_py3.py
@@ -18,7 +18,7 @@ beginDate = datetime.strptime('2017-01-01', '%Y-%m-%d')
 
 wb = xlwt.Workbook()
 
-cookies = chrome_cookies('http://127.0.0.1')
+# cookies = chrome_cookies('http://127.0.0.1')
 
 response = requests.request('GET', 'http://www.baidu.com', headers={}, params={}, cookies=cookies)
 
